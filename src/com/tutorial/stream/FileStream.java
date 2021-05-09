bc5863f00d614317fae7c025051ccee5eb631e96
hediye853@gmail.com <hediye853@gmail.com>
2021-05-09 14:39:40 +0430

    1- File Stream Test

---
+++ src/com/tutorial/stream/FileStream.java
@@ -0,0 +1,53 @@
+package com.tutorial.stream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/*
+*  file => stream
+* close fle is important
+* */
+public class FileStream {
+    public static void main(String[] args) {
+        //File.readFile("F:\\java_tutorial\\functionalPrograming\\src\\test.txt");
+        //File.readFile("src/test.txt");
+        //File.getListFileTree("");
+    }
+
+    public static class File{
+        public static void readFile(String pathFile){
+            if(pathFile != null){
+                Path path = Paths.get(pathFile);
+                if(Files.exists(path)){
+//                    try with source
+                    try(Stream<String> stringStream = Files.lines(path)) {
+                        stringStream.forEach(System.out::println);
+                    } catch (IOException e) {
+                        System.out.println("Exception for reading file");
+                    }
+                }else{
+                    System.out.println("File not exist");
+                }
+            }else{
+                System.out.println("Path of file not exist");
+            }
+        }
+
+        public static void getListFileTree(String path){
+            if(path != null){
+                Path dir = Paths.get(path);
+                System.out.println("----dir-----"+dir.toAbsolutePath());
+                try(Stream<Path> dircts= Files.walk(dir)){
+                    dircts.forEach(System.out::println);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("Path not exist");
+            }
+        }
+    }
+}
--- src/com/tutorial/stream/StreamExamples.java
+++ src/com/tutorial/stream/StreamExamples.java
@@ -3,17 +3,27 @@ package com.tutorial.stream;
 import java.math.BigInteger;
 import java.util.stream.Stream;
 
+
+/*
+* 1- iterator
+* 2- generate
+*
+* */
 public class StreamExamples {
     public static void main(String[] args) {
         Stream.iterate(0L, value -> 1L + value)
-                .filter(value -> value < 10)
-                .filter(value -> value % 2 == 0)
-                .forEach(System.out::println);
+                .filter(value -> value < 10);
+//                .filter(value -> value % 2 == 0)
+//                .forEach(System.out::println);
 
-
+        System.out.println("*******************************");
         Stream.iterate(BigInteger.ONE,bigInteger -> bigInteger.multiply(BigInteger.ONE))
                 .limit(5)
                 .forEach(System.out::println);
+
+        Stream.generate(() -> 12).limit(3).forEach(System.out::println);
+
+        Stream.generate(Math::random).limit(3).forEach(System.out::println);
 
 
     }
