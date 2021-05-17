d7dd36e53ab20f8040ebeb3383076be93a114164
hediye853@gmail.com <hediye853@gmail.com>
2021-05-17 20:10:31 +0430

    Lambda Test

---
+++ src/com/tutorial/lambda/Lambda.java
@@ -0,0 +1,14 @@
+package com.tutorial.lambda;
+
+public class Lambda {
+    public static void main(String[] args) {
+        StringToInt stringToInt = (String str) -> str.length();//String::length
+        Integer length = stringToInt.map("Hi Lambda!");
+        System.out.println("Length: "+length);
+    }
+
+    @FunctionalInterface
+    public interface StringToInt {
+        Integer map(String str);
+    }
+}
